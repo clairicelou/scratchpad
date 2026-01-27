c5c7e3d183cfaddaec91c3a250302c36a7934372
ds4e <trj2j@virginia.edu>
2026-01-27 10:41:11 -0500

    More changes for recording

--- py_20260127.py
+++ py_20260127.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import skimpy as skim 
+
+print("Did you detect these changes?")
 
 df = pd.read_csv('./data/craiglist_cville_cars_long.csv')
 df.head()
